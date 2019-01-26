9e5a61b447fa019c944f597b735c6cc4acf37246
浅忆梦微凉 <3538922590@qq.com>
2019-01-26 21:16:43 +0800

    解决3

--- 剑指offer16_数值的整数次方/test.cpp
+++ 剑指offer16_数值的整数次方/test.cpp
@@ -51,10 +51,60 @@ void TestFunc2(){
 	cout << "2.22的4次方为：" << result << endl;
 }
 
+class Solution3 {
+public:
+	double Power(double base, int exponent) {
+		if (exponent == 0){
+			return 1;
+		}
+		else if (exponent > 0){
+			if (base == 0.0){
+				return 0;
+			}
+			if (base == 1){
+				return 1;
+			}
+			if (base != 0 && base != 1){
+				double result = base;
+				int i = exponent;
+				while (i - 1){
+					result *= base;
+					--i;
+				}
+				return result;
+			}
+		}
+		else if (exponent < 0){
+			if (base == 0.0){
+				return 0;
+			}
+			if (base == 1){
+				return 1;
+			}
+			if (base != 0 && base != 1){
+				double temp = 1 / base;
+				double result = temp;
+				int i = exponent;
+				while (i - 1){
+					result *= temp;
+					++i;
+				}
+				return result;
+			}
+		}
+	}
+};
+
+void TestFunc3(){
+	Solution3 s;
+	double result = s.Power(2, -3);
+	cout << "2.22的4次方为：" << result << endl;
+}
 
 int main(){
-	TestFunc1();
-	TestFunc2();
+	/*TestFunc1();
+	TestFunc2();*/
+	TestFunc3();
 	system("pause");
 	return 0;
 }
