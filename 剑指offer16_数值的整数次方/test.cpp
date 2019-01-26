d5c39beb960197e6e83a31f7bfb8dfb10a77fe88
浅忆梦微凉 <3538922590@qq.com>
2019-01-26 21:00:08 +0800

    答案错误

--- 剑指offer16_数值的整数次方/test.cpp
+++ 剑指offer16_数值的整数次方/test.cpp
@@ -15,14 +15,41 @@ public:
 	}
 };
 
-void TestFunc(){
+void TestFunc1(){
 	Solution s;
 	double result = s.Power(2.22, 4);
 	cout << "2.22的4次方为：" << result << endl;
 }
 
+class Solution2 {
+public:
+	double Power(double base, int exponent) {
+		if (exponent == 0){
+			return 1.0;
+		}
+		if (exponent < 0 && base == 0){
+			return 0.0;
+		}
+		if (base != 0){
+			int i = exponent;
+			double result = base;
+			while (i-1){
+				result *= base;
+			}
+			return result;
+		}
+	}
+};
+
+void TestFunc2(){
+	Solution2 s;
+	double result = s.Power(2.22, 4);
+	cout << "2.22的4次方为：" << result << endl;
+}
+
+
 int main(){
-	TestFunc();
+	TestFunc1();
 	system("pause");
 	return 0;
 }
